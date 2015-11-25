45c954d01ab2700b92dc242d25df96ec6e2fe853
Sindre Sorhus <sindresorhus@gmail.com>
2015-11-25 09:53:38 +0700

    update CSS classes for messenger changes

--- browser.js
+++ browser.js
@@ -3,9 +3,9 @@ const ipc = require('electron').ipcRenderer;
 
 ipc.on('show-preferences', () => {
 	// create the menu for the below
-	document.querySelector('._150g._30yy._2fug._p').click();
+	document.querySelector('._30yy._2fug._p').click();
 
-	document.querySelector('._54nq._2i-c._150g._558b._2n_z li:first-child a').click();
+	document.querySelector('._54nq._2i-c._558b._2n_z li:first-child a').click();
 });
 
 ipc.on('new-conversation', () => {
@@ -14,7 +14,7 @@ ipc.on('new-conversation', () => {
 
 ipc.on('log-out', () => {
 	// create the menu for the below
-	document.querySelector('._150g._30yy._2fug._p').click();
+	document.querySelector('._30yy._2fug._p').click();
 
-	document.querySelector('._54nq._2i-c._150g._558b._2n_z li:last-child a').click();
+	document.querySelector('._54nq._2i-c._558b._2n_z li:last-child a').click();
 });
