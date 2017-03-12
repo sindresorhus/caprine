2ca3785fa846b1acc989dc01152552320c06a9f9
Dang <dang_huynh@compex.com.sg>
2017-03-13 03:14:23 +0800

    support retina display
    correct dpr cookie to screen scale factor

--- index.js
+++ index.js
@@ -53,6 +53,24 @@ function updateBadge(title) {
 	}
 }
 
+function enableHiresResources() {
+	const scaleFactor = Math.max(electron.screen.getAllDisplays().map(scr => scr.scaleFactor))
+	if (scaleFactor == 1) return;
+
+	electron.session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+		let cookie = details.requestHeaders.Cookie
+		if (cookie && details.method === 'GET' && !details.url.indexOf('https://www.messenger.com/')) {
+			if (cookie.match(/(; )?dpr=\d/)) {
+				cookie = cookie.replace(/dpr=\d/, `dpr=${scaleFactor}`);
+			} else {
+				cookie = `${cookie}; dpr=${scaleFactor}`;
+			}
+			details.requestHeaders.Cookie = cookie;
+		}
+		callback({cancel: false, requestHeaders: details.requestHeaders});
+	});
+}
+
 function createMainWindow() {
 	const lastWindowState = config.get('lastWindowState');
 	const isDarkMode = config.get('darkMode');
@@ -115,6 +133,8 @@ app.on('ready', () => {
 	electron.Menu.setApplicationMenu(appMenu);
 	mainWindow = createMainWindow();
 	tray.create(mainWindow);
+
+	enableHiresResources();
 
 	const page = mainWindow.webContents;
 
