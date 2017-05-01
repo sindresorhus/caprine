853086246605d225c735cd8578e5dc7929004b85
Sindre Sorhus <sindresorhus@gmail.com>
2017-05-01 20:17:12 +0700

    Only set the window to transparent on macOS
    We only need it for the vibrancy mode, which is only available on macOS.
    
    Fixes #178
    Closes #180

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function createMainWindow() {
 		titleBarStyle: 'hidden-inset',
 		autoHideMenuBar: true,
 		darkTheme: isDarkMode, // GTK+3
-		transparent: true,
+		transparent: process.platform === 'darwin',
 		webPreferences: {
 			preload: path.join(__dirname, 'browser.js'),
 			nodeIntegration: false,
