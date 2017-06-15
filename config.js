5ce15e48837db6d7e1245476eac0e179348f2212
Sindre Sorhus <sindresorhus@gmail.com>
2017-06-15 16:20:15 +0200

    `electron-config` → `electron-store`

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
-const Config = require('electron-config');
+const Store = require('electron-store');
 
-module.exports = new Config({
+module.exports = new Store({
 	defaults: {
 		darkMode: false,
 		vibrancy: false,
