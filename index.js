3dcef247f7ade6965144063795763346ceab3bc5
Sindre Sorhus <sindresorhus@gmail.com>
2017-09-29 14:45:09 +0700

    Temporary workaround for #295

--- index.js
+++ index.js
@@ -154,7 +154,8 @@ function createMainWindow() {
 		minWidth: 400,
 		minHeight: 200,
 		alwaysOnTop: config.get('alwaysOnTop'),
-		titleBarStyle: 'hidden-inset',
+		// Temp workaround for macOS High Sierra, see #295
+		titleBarStyle: process.platform === 'darwin' && Number(require('os').release().split('.')[0]) >= 17 ? null : 'hidden-inset',
 		autoHideMenuBar: true,
 		darkTheme: isDarkMode, // GTK+3
 		webPreferences: {
