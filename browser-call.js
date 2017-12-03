57d539d72f77b6b989a74b37b10cb92943e1e07e
Vadim Demedes <vdemedes@gmail.com>
2017-12-03 17:57:18 +0200

    fixes

--- browser-call.js
+++ browser-call.js
@@ -1,23 +1,4 @@
-'use strict';
-const electron = require('electron');
-
-const {ipcRenderer: ipc} = electron;
-
-const onReady = fn => {
-	window.addEventListener('load', () => {
-		const check = () => {
-			if (document.querySelector('._3quh._30yy._2t_')) {
-				fn();
-			} else {
-				setTimeout(check, 100);
-			}
-		};
-
-		check();
-	});
-}
-
-onReady(() => {
-	// Start call
-	document.querySelector('._3quh._30yy._2t_').click();
+(async () => {
+	const startCallButton = await elementReady('._3quh._30yy._2t_');
+	startCallButton.click();
 });
