sindresorhus/caprine

commit 5054d6a3d33bb20340853aaec6d2803589c21876

Open conversations from the Dock menu (#470)

index.js
@@ -10,6 +10,7 @@ const isDev = require('electron-is-dev');
 const appMenu = require('./menu');
 const config = require('./config');
 const tray = require('./tray');
+const {sendAction} = require('./util');
 
 require('./touch-bar'); // eslint-disable-line import/no-unassigned-import
 
@@ -18,7 +19,7 @@ require('electron-dl')();
 require('electron-context-menu')();
 
 const domain = config.get('useWorkChat') ? 'facebook.com' : 'messenger.com';
-const {app, ipcMain, Menu} = electron;
+const {app, ipcMain, Menu, nativeImage} = electron;
 
 app.setAppUserModelId('com.sindresorhus.caprine');
 
@@ -232,17 +233,34 @@ app.on('ready', () => {
 	tray.create(mainWindow);
 
 	if (process.platform === 'darwin') {
-		dockMenu = electron.Menu.buildFromTemplate([
-			{
-				label: 'Mute Notifications',
-				type: 'checkbox',
-				checked: config.get('notificationsMuted'),
-				click() {
-					mainWindow.webContents.send('toggle-mute-notifications');
-				}
+		const firstItem = {
+			label: 'Mute Notifications',
+			type: 'checkbox',
+			checked: config.get('notificationsMuted'),
+			click() {
+				mainWindow.webContents.send('toggle-mute-notifications');
 			}
-		]);
+		};
+		dockMenu = electron.Menu.buildFromTemplate([firstItem]);
 		app.dock.setMenu(dockMenu);
+
+		ipcMain.on('conversations', (event, conversations) => {
+			if (conversations.length === 0) {
+				return;
+			}
+
+			mainWindow.show();
+			const items = conversations.map(({label, icon}, index) => {
+				return {
+					label: `${label}`,
+					icon: nativeImage.createFromDataURL(icon),
+					click: () => {
+						sendAction('jump-to-conversation', index + 1);
+					}
+				};
+			});
+			app.dock.setMenu(electron.Menu.buildFromTemplate([firstItem, {type: 'separator'}, ...items]));
+		});
 	}
 
 	enableHiresResources();

browser.js
@@ -326,41 +326,24 @@ function closePreferences() {
 	const doneButton = document.querySelector('._3quh._30yy._2t_._5ixy');
 	doneButton.click();
 }
-
-function initTouchBar() {
+async function sendConversationList() {
 	const sidebar = document.querySelector('[role=navigation]');
 
-	const sendTouchBar = async () => {
-		const conversations = [];
-		for (const el of sidebar.querySelectorAll('._1ht1')) {
-			conversations.push({
+	const conversations = await Promise.all(
+		[...sidebar.querySelectorAll('._1ht1')]
+			.splice(0, 10)
+			.map(async el => ({
 				label: el.querySelector('._1ht6').textContent,
 				selected: el.classList.contains('_1ht2'),
-				icon: await getDataUrlFromImg( // eslint-disable-line no-await-in-loop
+				unread: el.classList.contains('_1ht3'),
+				icon: await getDataUrlFromImg(
 					el.querySelector('._55lt img'),
 					el.classList.contains('_1ht3')
 				)
-			});
+			}))
+	);
 
-			if (conversations.length >= 15) {
-				break;
-			}
-		}
-
-		if (conversations.length > 0) {
-			ipc.send('touch-bar', conversations);
-		}
-	};
-
-	sendTouchBar();
-
-	const conversationListObserver = new MutationObserver(sendTouchBar);
-	conversationListObserver.observe(sidebar, {
-		subtree: true,
-		childList: true,
-		attributes: true,
-		attributeFilter: ['class']
-	});
+	ipc.send('conversations', conversations);
 }
 
 // Return canvas with rounded image
@@ -457,7 +440,17 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 window.addEventListener('load', () => {
-	initTouchBar();
+	const sidebar = document.querySelector('[role=navigation]');
+
+	sendConversationList();
+
+	const conversationListObserver = new MutationObserver(sendConversationList);
+	conversationListObserver.observe(sidebar, {
+		subtree: true,
+		childList: true,
+		attributes: true,
+		attributeFilter: ['class']
+	});
 });
 
 // It's not possible to add multiple accelerators

touch-bar.js
@@ -4,7 +4,7 @@ const {sendAction, getWindow} = require('./util');
 
 const {TouchBarButton} = TouchBar;
 
-ipc.on('touch-bar', (event, conversations) => {
+ipc.on('conversations', (event, conversations) => {
 	const touchBar = new TouchBar(
 		conversations.map(({label, selected, icon}, index) => {
 			return new TouchBarButton({

commit 3bbb034a52cd94af87fddfd39647323eb0a2a6e7

Don’t show the main window on conversations change
Fixes #483 (a bug introduced in #470)

index.js
@@ -249,7 +249,6 @@ app.on('ready', () => {
 				return;
 			}
 
-			mainWindow.show();
 			const items = conversations.map(({label, icon}, index) => {
 				return {
 					label: `${label}`,