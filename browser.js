a2e1301c508da47bc8071676af81a5f51ab6ee63
Nigel Rodriguez <anr.rod@gmail.com>
2018-10-31 16:05:30 +0800

    Fix dark mode + vibrancy not rendering properly on restart

--- browser.js
+++ browser.js
@@ -439,7 +439,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// Prevent flash of white on startup when in dark mode
 	// TODO: find a CSS-only solution
-	if (config.get('darkMode')) {
+	if (config.get('darkMode') && !config.get('vibrancy')) {
 		document.documentElement.style.backgroundColor = '#192633';
 	}
 
