259000cad4d94b2db1101924818c48b248b14af2
mikigal <mikigal.priv@gmail.com>
2019-01-14 18:59:35 +0100

    Fix like emoji on sidebar

--- emoji.js
+++ emoji.js
@@ -5,7 +5,8 @@ module.exports = {
 	getEmoji(url) {
 		const array = url.split('/');
 		let type = array[6];
-		type = config.get('emojiStyle') + type.substr(1, type.length);
+
+		type = (url.endsWith('f0000.png') ? 'z' : config.get('emojiStyle')) + type.substr(1, type.length); // Fix like emoji on sidebar, it's available only i 'z'
 		return url.replace('v9', 'v8').replace(array[6], type);
 	}
 };
