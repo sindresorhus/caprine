c807d0a6c7b692431bbadfe414a3a152dea57e37
David Wu <wu.david3283@gmail.com>
2019-04-25 14:09:04 +1000

    Add video call width, height and aspect ratio

--- source/browser-call.ts
+++ source/browser-call.ts
@@ -1,6 +1,23 @@
+// @ts-ignore
+import {ipcRenderer as ipc, Event as ElectronEvent} from 'electron';
 import elementReady from 'element-ready';
 
+const videoSelector = '._chh>video';
+
 (async () => {
-	const startCallButton = await elementReady<HTMLElement>('._3quh._30yy._2t_');
-	startCallButton.click();
+    const startCallButton = await elementReady<HTMLElement>('._3quh._30yy._2t_');
+    startCallButton.click();
+})();
+
+(async function withVideoCall() {
+    function onVideoDimensionsLoaded(e: Event) {
+        const target = <HTMLVideoElement>e.target;
+        // const aspectRatioData = {height: target.videoHeight, width: target.videoWidth};
+        console.log(target);
+        ipc.send('set-video-call-aspect-ratio', target.videoWidth, target.videoHeight)
+    }
+
+    const videoElement = await elementReady<HTMLElement>(videoSelector);
+    videoElement.addEventListener('loadedmetadata', onVideoDimensionsLoaded);
+    videoElement.addEventListener('playing', onVideoDimensionsLoaded)
 })();
