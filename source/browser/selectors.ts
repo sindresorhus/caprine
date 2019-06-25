08f77954fa608e5c7d7953c81c0cddc66ffcdc13
Daniel Andrei <andrei.daniel26@gmail.com>
2019-06-26 01:38:20 +0300

    Update conversationSelector for scrollbar dragging when zooming

--- source/browser/selectors.ts
+++ source/browser/selectors.ts
@@ -1,4 +1,4 @@
 export default {
 	conversationList: 'div[role="navigation"] > div > ul',
-	conversationSelector: '._4u-c._1wfr > ._5f0v.uiScrollableArea'
+	conversationSelector: '._4u-c._1wfr .__i_, ._4u-c._1wfr #conversationWindow'
 };
