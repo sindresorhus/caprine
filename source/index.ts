fa6988e60b79c64fc55f23578f45a4cca3ad76ae
Jarek Radosz <jradosz@gmail.com>
2019-08-30 11:30:07 +0200

    Work around the vibrancy issue
    Vibrancy effect is still missing
    - when docked Dev Tools are present
    - in a brief time window (~3 seconds) after a navigation/reload

--- source/index.ts
+++ source/index.ts
@@ -469,6 +469,7 @@ function createMainWindow(): BrowserWindow {
 
 if (is.macos) {
 	ipcMain.on('set-vibrancy', () => {
+		mainWindow.setBackgroundColor('#00000000'); // Transparent
 		mainWindow.setVibrancy('sidebar');
 
 		if (config.get('followSystemAppearance')) {
