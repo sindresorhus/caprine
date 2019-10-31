2b2be200db844d6319800da24f2bd7cd4d478c9a
MHekert <32526320+MHekert@users.noreply.github.com>
2019-10-31 08:59:37 +0100

    Fix `Always on Top` after tray toggling on KDE Plasma (#1150)

--- source/tray.ts
+++ source/tray.ts
@@ -20,6 +20,11 @@ export default {
 				win.hide();
 			} else {
 				win.show();
+
+				// Workaround for https://github.com/electron/electron/issues/20858
+				// `setAlwaysOnTop` stops working after hiding the window on KDE Plasma.
+				const alwaysOnTopMenuItem = Menu.getApplicationMenu()!.getMenuItemById('always-on-top');
+				win.setAlwaysOnTop(alwaysOnTopMenuItem.checked);
 			}
 		}
 
