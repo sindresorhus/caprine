3beef2f50d9790163ee2884c858b5f64cba9d1e1
gohlinka2 <gohlinka2@gmail.com>
2020-01-05 22:38:30 +0100

    Added icon paths

--- source/constants.ts
+++ source/constants.ts
@@ -1,3 +1,6 @@
 import * as path from 'path';
 
 export const caprineIconPath = path.join(__dirname, '..', 'static', 'Icon.png');
+export const nextIconPath = path.join(__dirname, '..', 'static', 'next.png');
+export const previousIconPath = path.join(__dirname, '..', 'static', 'previous.png');
+
