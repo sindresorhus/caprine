fd1a60c29608b12e8e45e793699f27ee6ec2b6a1
Tobias Østergren <tobias.oestergren@ieee.org>
2020-03-08 20:44:54 +0100

    Reverted unintentional, irrelevant change and removed tray meny element.

--- source/tray.ts
+++ source/tray.ts
@@ -73,14 +73,6 @@ export default {
 					toggleWindow();
 				}
 			},
-			{
-				label: 'Reset Position',
-				visible: !is.macos,
-				click() {
-					win.setPosition(0, 0);
-				}
-
-			},
 			...macosMenuItems,
 			{
 				type: 'separator'
