0e5175bc1983249af0e99105079bc6109470d958
Dušan Simić <dusan.simic1810@gmail.com>
2021-02-07 06:58:56 +0100

    Fix Search in Conversation menu option (#1546)

--- source/browser/selectors.ts
+++ source/browser/selectors.ts
@@ -3,5 +3,6 @@ export default {
 	conversationListNewDesign: '[role=navigation] [role=grid]',
 	conversationSelector: '._4u-c._1wfr .__i_, ._4u-c._1wfr #conversationWindow',
 	conversationSelectorNewDesign: '[aria-label=Messages]',
-	notificationCheckbox: '._374b:nth-of-type(4) ._4ng2 input'
+	notificationCheckbox: '._374b:nth-of-type(4) ._4ng2 input',
+	rightSidebarButtons: '.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.cbu4d94t.g5gj957u.f4tghd1a.ifue306u.kuivcneq.t63ysoy8 [role=button]'
 };
