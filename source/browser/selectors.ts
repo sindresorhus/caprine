d5497763c0dd9c8896f7359f62601219f9a9106b
Dušan Simić <dusan.simic1810@gmail.com>
2021-02-16 12:08:02 +0100

    Fix shortcuts on non US locales

--- source/browser/selectors.ts
+++ source/browser/selectors.ts
@@ -2,7 +2,7 @@ export default {
 	conversationList: 'div[role="navigation"] > div > ul',
 	conversationListNewDesign: '[role=navigation] [role=grid]',
 	conversationSelector: '._4u-c._1wfr .__i_, ._4u-c._1wfr #conversationWindow',
-	conversationSelectorNewDesign: '[aria-label=Messages]',
+	conversationSelectorNewDesign: '[role=main] [role=grid]',
 	notificationCheckbox: '._374b:nth-of-type(4) ._4ng2 input',
 	rightSidebarButtons: '.rq0escxv.l9j0dhe7.du4w35lb.j83agx80.cbu4d94t.g5gj957u.f4tghd1a.ifue306u.kuivcneq.t63ysoy8 [role=button]'
 };
