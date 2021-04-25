ee72ec1d6e5688b71fad60420a0ab8d9f2f10fa4
Michael Quevillon <mquevill@nd.edu>
2021-04-25 10:40:45 -0400

    Improve a type annotation (#1616)

--- source/emoji.ts
+++ source/emoji.ts
@@ -299,8 +299,7 @@ with this: https://static.xx.fbcdn.net/images/emoji.php/v9/z27/2/32/1f600.png
                                                  (see here) ^
 */
 export async function process(url: string): Promise<Response> {
-	// TODO: Remove the `any` here.
-	const emojiStyle = config.get('emojiStyle') as any;
+	const emojiStyle = config.get('emojiStyle') as EmojiStyle;
 	const emojiSetCode = codeForEmojiStyle(emojiStyle);
 
 	// The character code is the filename without the extension.
