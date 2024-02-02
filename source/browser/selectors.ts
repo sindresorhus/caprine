a6eb051c9f62b15cce576edfa7e89d7412fe6839
Stratos Karakondis <st.krknds@gmail.com>
2024-02-02 18:20:07 +0200

    better selector

--- source/browser/selectors.ts
+++ source/browser/selectors.ts
@@ -1,6 +1,6 @@
 export default {
 	leftSidebar: '[role="navigation"] > div > div', // ! Tray icon dependency
-	chatsIcon: '[data-pagelet="MWCMInboxLeftRailSidebar"] > div > div > div > span:nth-child(1) > a', // ! Tray icon dependency
+	chatsIcon: '[data-pagelet="MWCMInboxLeftRailSidebar"] > div > div > div:nth-child(1) a', // ! Tray icon dependency
 	conversationList: '[role=navigation] [role=grid] [class="x1n2onr6"]',
 	conversationSelector: '[role=main] [role=grid]',
 	conversationSidebarUnreadDot: 'x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x78zum5 x1a2a7pz',
