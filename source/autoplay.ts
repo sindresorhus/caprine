d1b2e14b17ae0ec6a75d47967a24c891c481199d
Michael J Brancato <usmccoder@gmail.com>
2024-02-29 15:19:45 -0700

    chore: Package version bumps and lint ignores (#2136)

--- source/autoplay.ts
+++ source/autoplay.ts
@@ -139,9 +139,7 @@ const conversationDivObserver = new MutationObserver(_ => {
 	let conversation = document.querySelector(`#${conversationId}`);
 
 	// Fetch it using `querySelector` if no luck with the `conversationId`
-	if (!conversation) {
-		conversation = document.querySelector(selectors.conversationSelector);
-	}
+	conversation ||= document.querySelector(selectors.conversationSelector);
 
 	// If we have a new reference
 	if (conversation && conversationWindow !== conversation) {
